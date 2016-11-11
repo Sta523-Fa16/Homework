fe9aaaa9aa046baf948c4496311d54b760af9c51
Colin Rundel <rundel@gmail.com>
2016-11-10 19:45:54 -0500

    Fix pp

--- hw6/update_score.R
+++ hw6/update_score.R
@@ -15,7 +15,7 @@ if (!file.exists("precincts.json"))
 pred = gBuffer(readOGR("precincts.json", layer="OGRGeoJSON", verbose=FALSE),
                width=0, byid=TRUE)
 
-load(file = "pp.Rdata")
+load(file = "pp_fa16.Rdata")
 pp = spTransform(pp, CRS("+proj=longlat +datum=WGS84"))
 
 
